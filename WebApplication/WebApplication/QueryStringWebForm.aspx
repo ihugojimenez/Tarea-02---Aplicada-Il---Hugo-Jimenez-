4266568c74c8a5ce3404728674f0531b144e8a38
ihugojimenez <hugofernando809@hotmail.com>
2016-09-20 10:28:00 -0400

    No pude con el stringquery

---
+++ WebApplication/WebApplication/QueryStringWebForm.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QueryStringWebForm.aspx.cs" Inherits="WebApplication.QueryStringWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication/WebApplication/QueryStringWebForm.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class QueryStringWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 1
+            // Get collection
+            NameValueCollection n = Request.QueryString;
+            // 2
+            // See if any query string exists
+            if (n.HasKeys())
+            {
+                // 3
+                // Get first key and value
+                string k = n.GetKey(0);
+                string v = n.Get(0);
+                // 4
+                // Test different keys
+                if (k == "param")
+                {
+                    Response.Write("param is " + v);
+                }
+                if (k == "id")
+                {
+                    Response.Write("id is " + v);
+                }
+            }
+        }
+    }
+}
